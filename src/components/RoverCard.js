39752457b5a3be1fb84376a169a98e681f643ec9
kai-blt <blaitong@gmail.com>
2020-10-07 15:40:50 -0700

    Completed rover component

--- src/components/RoverCard.js
+++ src/components/RoverCard.js
@@ -1,21 +1,23 @@
 import React from 'react'
-
+import './RoverCard.css'
 /*RoverCard takes data from the Mars Rover Photos API and appends to DOM. 
 */
 export default function RoverCard(props) {
     const { data } = props;
     return (
         <>
-            <div className="imageCardContainer">
+            <div className="roverCardContainer">
+                <div><h2>Rover Camera</h2><span>{data[0].earth_date}</span></div>                
                 {data.map(photo => {
                     return (
                         <>
-                            <h3>{photo.camera.full_name}</h3>
-                            <img key={photo.id} src={photo.img_src}/>
+                            <div>
+                                <h3>{photo.camera.full_name}</h3>
+                                <img key={photo.id} src={photo.img_src}/>
+                            </div>
                         </>
                     )
-                })}
-                
+                })}                
             </div>           
         </>
     )
